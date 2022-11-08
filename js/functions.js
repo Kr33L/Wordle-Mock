f8e38265bf89da3c0d02821e3746704d74aca6c1
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-11-08 16:06:39 +0000

    second attempt

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,17 @@
 function loadScript(src) {
-  const script = document.createElement('script');
-  script.src = src;
-  document.body.appendChild(script);
-};
+	const script = document.createElement("script");
+	script.src = src;
+	document.body.appendChild(script);
+}
 
-loadScript('js/words.js');
-setTimeout(() => { loadScript('js/main.js'); });
+loadScript("js/words.js");
+setTimeout(() => {
+	loadScript("js/main.js");
+});
+
+//get a random word from the dictionary array
+
+function getRandomWord() {
+	const randomIndex = Math.floor(Math.random() * dictionary.length);
+	return dictionary[randomIndex];
+}
