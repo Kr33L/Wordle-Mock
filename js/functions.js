906560358b341cfd698aaa692891787477a3d9ce
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-11-09 17:26:41 +0000

    storage, getRandomWord, setLocalStorage, getWord, dateFormat

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,52 @@
-function loadScript(src) {
-	const script = document.createElement("script");
-	script.src = src;
-	document.body.appendChild(script);
-}
-
-loadScript("js/words.js");
-setTimeout(() => {
-	loadScript("js/main.js");
-});
-
-//get a random word from the dictionary array
+const storage = {
+	lastUpdatedHuman: localStorage.getItem("lastUpdatedHuman"),
+	randomWord: localStorage.getItem("randomWord"),
+	lastUpdated: localStorage.getItem("lastUpdated"),
+};
 
 function getRandomWord() {
-	const randomIndex = Math.floor(Math.random() * dictionary.length);
-	return dictionary[randomIndex];
+	const randomIndex = Math.floor(Math.random() * dictionary.length); //gets a random index of the dictionary array
+	const randomWord = dictionary[randomIndex]; //assign the random indexed word to a variable
+	return randomWord; // return the random word
+}
+
+function dateFormat() {
+	const date = new Date();
+	const year = date.getFullYear();
+	const month = date.getMonth();
+	const day = date.getDate();
+	const hour = date.getHours();
+	if (hour < 10) {
+		hour = "0" + hour;
+	}
+	const minute = date.getMinutes();
+	if (minute < 10) {
+		minute = "0" + minute;
+	}
+	const currentDate = `Last refresh was on ${day}/${month}/${year} at ${hour}:${minute}`;
+	return currentDate;
+}
+
+function timeUntilRefresh() {
+  const lastUpdated = localStorage.getItem("lastUpdated");
+  const timeUntilRefresh = 86400000 - (new Date() - lastUpdated);
+  return timeUntilRefresh;
+}
+
+function setLocalStorage() {
+	console.log(storage);
+	localStorage.setItem("lastUpdated", Date.now());
+	localStorage.setItem("lastUpdatedHuman", dateFormat());
+  localStorage.setItem("timeUntilRefresh", timeUntilRefresh());
+	localStorage.setItem("targetWord", getRandomWord());
+	console.log(storage);
+}
+
+function getWord() {
+	const targetWord = localStorage.getItem("targetWord");
+	const lastUpdated = localStorage.getItem("lastUpdated");
+	//if local storage is empty, or if the word is older than 24 hours, get a new word
+	if (targetWord === null || lastUpdated === null || Date.now() - lastUpdated > 86400000) {
+		setLocalStorage();
+	}
 }
--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log(dictionary);
+(function loadScripts() {
+	const scripts = ["js/words.js", "js/functions.js"];
+	for (const i of scripts) {
+		const script = document.createElement("script");
+		script.src = i;
+		document.body.appendChild(script);
+	}
+})();
+
+//wait until window has fully loaded
+window.addEventListener("load", function () {
+  getWord();
+});
